c2d450f063c437c97f306e5f6acc540c6c3b6682
Lukas Heuberger <45766540+lukasheuberger@users.noreply.github.com>
2023-03-15 16:43:20 +0100

    circle detection added to GUI

--- vesicleimaging/GUI.py
+++ vesicleimaging/GUI.py
@@ -87,6 +87,7 @@ class GUIFunctions():
         self.LoadImageData()
         # self.SaveImages()
         self.PlotImage()
+
     # todo determine somewhere if data is czi or hdf5 and act accordingly (already does it but would like to be more elegant)
     def ChooseInputHDF5Folder(self):
         # self.sourcefolder = str(QFileDialog.getExistingDirectory(None, "Select Directory"))
@@ -94,7 +95,6 @@ class GUIFunctions():
         print(self.sourcefolder)
         self.filetype = 'hdf5'
         self.Initialize()
-
 
     def OpenFolder(self):
         self.files, self.filenames = cih.get_files(self.sourcefolder)
@@ -108,7 +108,7 @@ class GUIFunctions():
     def LoadChannels(self, index=0):
         index = self.ImageSelector.currentIndex()
         try:
-            #for ix, amd in enumerate(self.add_metadata[0]):
+            # for ix, amd in enumerate(self.add_metadata[0]):
             channels = cih.get_channels([self.add_metadata[index]])  # this also needs to change when image changes
 
             self.channel_names = []
@@ -124,9 +124,15 @@ class GUIFunctions():
 
             self.PlotChannel.clear()
             self.PlotChannel.addItems(self.channel_names)
+
+            self.DetectDisplayChannel.clear()
+            self.DetectDisplayChannel.addItems(self.channel_names)
+
+            self.DetectDetectChannel.clear()
+            self.DetectDetectChannel.addItems(self.channel_names)
+
         except AttributeError:
             pass
-
 
     def LoadImageData(self):
 
@@ -154,8 +160,7 @@ class GUIFunctions():
 
         self.ImageSelector.addItems(self.filenames)
 
-
-        self.LoadChannels()#index=0)
+        self.LoadChannels()  # index=0)
 
         # todo probably some of this can be their own functions
 
@@ -177,7 +182,7 @@ class GUIFunctions():
             print('timelapse!')
 
             self.timelapse_frames = self.img_reduced[self.ImageSelector.currentIndex()].shape[1]
-            self.Timepoint_Slider.setMaximum(self.timelapse_frames-1)
+            self.Timepoint_Slider.setMaximum(self.timelapse_frames - 1)
             self.Timepoint_Slider.setEnabled(True)
 
         if self.img_reduced[self.ImageSelector.currentIndex()].shape[2] > 1:
@@ -190,33 +195,21 @@ class GUIFunctions():
 
         self.ImageType.setText(self.image_type)
 
-        # self.channel = self.img[0][1]
-        # print(self.img[0].shape)
-        # mpl.image.imsave('name.png', self.channel)
-
     def SaveImagesToPNG(self):
         for ix, img in enumerate(self.img_reduced):
             cia.plot_images(img, self.metadata[ix], self.add_metadata[ix], saving=True, display=False)
 
-
-# todo make possible to load from HDF5
+    # todo make possible to load from HDF5
     def SaveImagesToHDF5(self):
         cih.save_files(self.img_reduced, self.metadata, self.add_metadata)
 
-    #todo make possible to save xml metadata
+    # todo make possible to save xml metadata
 
     def PlotImage(self):
-        ic(self.PlotChannel.currentText())
-        ic(self.ImageSelector.currentText())
-        ic(self.ImageSelector.currentIndex())
-        ic(self.Timepoint_Slider.sliderPosition())
-        # fig, axs = plt.subplots(len(self.img), 3, figsize=(15, 15))
-        # # , facecolor='w', edgecolor='k')
-        # fig.tight_layout(pad=2)
-        # fig.subplots_adjust(hspace=.15, wspace=.2)
-        # axs = axs.ravel()
-        # print(axs.shape)
-
+        # ic(self.PlotChannel.currentText())
+        # ic(self.ImageSelector.currentText())
+        # ic(self.ImageSelector.currentIndex())
+        # ic(self.Timepoint_Slider.sliderPosition())
 
         try:
             temp_filename = self.metadata[0]['Filename'].replace('.czi', '')
@@ -225,38 +218,32 @@ class GUIFunctions():
 
         ic(temp_filename)
 
-        #title_filename = ''.join([temp_filename, '_',
-                                  #channel_names[channel_index], '_t',
-                                  #str(timepoint_index), '_z',
-                                  #str(zstack_index)])
         scaling_x = cih.disp_scaling([self.add_metadata[self.ImageSelector.currentIndex()]])
-        ic(scaling_x)
-
-
-
+        # ic(scaling_x)
 
         plt.cla()  # clears plot
         self.ax = self.figure.add_subplot(111, frameon=False)
         plt.tight_layout(pad=0)
         self.ax.set_title(temp_filename)
-        #self.ax.axes.xaxis.set_ticklabels([])
-        #self.ax.axes.yaxis.set_ticklabels([])
-        #plt.axis('off')
-        #if scalebar:  # 1 pixel = scale [m]
+        # self.ax.axes.xaxis.set_ticklabels([])
+        # self.ax.axes.yaxis.set_ticklabels([])
+        # plt.axis('off')
+        # if scalebar:  # 1 pixel = scale [m]
         scalebar_value = 50
-        scalebar = ScaleBar(dx=scaling_x[0], location='lower right', fixed_value=scalebar_value, fixed_units='µm', frameon=False, color='w')
+        scalebar = ScaleBar(dx=scaling_x[0], location='lower right', fixed_value=scalebar_value, fixed_units='µm',
+                            frameon=False, color='w')
 
         ic(self.img_reduced[self.ImageSelector.currentIndex()].shape)
 
         # self.ax = self.PlotCanvas.figure.subplots()
-        self.ax.imshow(self.img_reduced[self.ImageSelector.currentIndex()][self.PlotChannel.currentIndex()][self.Timepoint_Slider.sliderPosition()][self.ZPosition_Slider.sliderPosition()], cmap=self.CmapSelector.currentText())
+        self.currentImage = self.img_reduced[self.ImageSelector.currentIndex()]
+
+        self.ax.imshow(self.currentImage[self.PlotChannel.currentIndex()][self.Timepoint_Slider.sliderPosition()][self.ZPosition_Slider.sliderPosition()], cmap=self.CmapSelector.currentText())
         self.ax.add_artist(scalebar)
         # self.ax.set_axis_off() somehow doesn't work
 
-
         # hide x-axis
         self.ax.get_xaxis().set_visible(False)
-
         # hide y-axis
         self.ax.get_yaxis().set_visible(False)
 
@@ -284,9 +271,26 @@ class GUIFunctions():
         # filename_counter = filename_counter + 1
 
     def updateSliderLabel(self, value):
-        #todo make two sliders out of this
-        self.TimeSliderTimepoint.setText(str(value))
-        self.ZStackSliderPosition.setText(str(value))
+        #todo make all this naming consistent
+        if self.sender() == self.Timepoint_Slider:
+            self.TimeSliderTimepoint.setText(str(value))
+        elif self.sender() == self.ZPosition_Slider:
+            self.ZStackSliderPosition.setText(str(value))
+        elif self.sender() == self.Param1_Slider:
+            self.Param1_SliderValue.setText(str(value))
+        elif self.sender() == self.Param2_Slider:
+            self.Param2_SliderValue.setText(str(value))
+
+    def detectGUVs(self):
+
+        #todo make this work for multiple images
+
+        self.detected_circles = cia.detect_circles([self.currentImage], [self.metadata[self.ImageSelector.currentIndex()]],
+                                          param1_array=self.Param1_Slider.sliderPosition(),
+                                          param2_array=self.Param2_Slider.sliderPosition(),
+                                          minmax=[int(self.SizeMin.text()), int(self.SizeMax.text())],
+                                          display_channel=self.DetectDisplayChannel.currentIndex(),
+                                          detection_channel=self.DetectDetectChannel.currentIndex())
 
 
 class qtGUI(QDialog, GUIFunctions):
@@ -308,6 +312,7 @@ class qtGUI(QDialog, GUIFunctions):
         self.createImageSelectionBox()
         self.createOptionBox()
         self.createPlotOptionBox()
+        self.createDetectCircleBox()
 
         topLayout = QHBoxLayout()
 
@@ -320,9 +325,9 @@ class qtGUI(QDialog, GUIFunctions):
         mainLayout.addWidget(self.ImageSelectionBox, 3, 0, 1, 1)
         mainLayout.addWidget(self.OptionBox, 4, 0, 1, 1)
         mainLayout.addWidget(self.PlotOptionBox, 5, 1, 1, 1)
+        mainLayout.addWidget(self.DetectCircleBox, 5, 0, 1, 1)
 
         self.setLayout(mainLayout)
-
 
     def createLoadBox(self):
         """
@@ -358,7 +363,7 @@ class qtGUI(QDialog, GUIFunctions):
         self.DataLoadBox.setLayout(layout)
 
     def createSaveBox(self):
-        #todo only make this accessible if data is loaded
+        # todo only make this accessible if data is loaded
         self.SaveBox = QGroupBox('Data Handling')
 
         fileSaveButton = QPushButton("Save Images to png's")
@@ -373,7 +378,6 @@ class qtGUI(QDialog, GUIFunctions):
 
         self.SaveBox.setLayout(layout)
 
-
     def createPlotCanvas(self):
         """
         Function that creates a canvas to plot the data and loads a toolbar to edit the graph
@@ -383,8 +387,9 @@ class qtGUI(QDialog, GUIFunctions):
         layout = QVBoxLayout()
         self.PlotCanvasBox.setLayout(layout)
 
-        self.figure = plt.figure(figsize=(2, 2), facecolor='None', edgecolor='None', frameon=False)  # TODO make changeable
-        #self.figure.axis('off')
+        self.figure = plt.figure(figsize=(2, 2), facecolor='None', edgecolor='None',
+                                 frameon=False)  # TODO make changeable
+        # self.figure.axis('off')
 
         self.PlotCanvas = FigureCanvas(self.figure)
         # set up canvas
@@ -397,13 +402,12 @@ class qtGUI(QDialog, GUIFunctions):
 
         layout.addWidget(self.PlotCanvas)
 
-
     def createImageSelectionBox(self):
         self.ImageSelectionBox = QGroupBox('Image Selection')
 
         ImageSelector_Label = QLabel('Select Image')
         self.ImageSelector = QComboBox()
-        #self.ImageSelector.addItems(['0', '1'])
+        # self.ImageSelector.addItems(['0', '1'])
         # self.ImageSelector.currentIndexChanged.connect(self.LoadChannels(index=1))
         self.ImageSelector.currentIndexChanged.connect(self.LoadChannels)
         self.ImageSelector.currentIndexChanged.connect(self.DetermineImageType)
@@ -439,7 +443,6 @@ class qtGUI(QDialog, GUIFunctions):
 
         self.OptionBox.setLayout(layout)
 
-
     def createOutputBox(self):
         self.OutputBox = QGroupBox('Output')
 
@@ -449,21 +452,19 @@ class qtGUI(QDialog, GUIFunctions):
         ImageTypeLabel = QLabel('Image Type:')
         self.ImageType = QLabel('')
 
-
-        #todo display image type somewhere here
-        #todo only show this if image is actually timelapse
+        # todo display image type somewhere here
+        # todo only show this if image is actually timelapse
         TimeSliderLabel = QLabel('timepoint')
         self.Timepoint_Slider = QSlider(Qt.Orientation.Horizontal, self)
         self.Timepoint_Slider.setTickInterval(1)
         self.Timepoint_Slider.valueChanged.connect(self.updateSliderLabel)
         self.Timepoint_Slider.valueChanged.connect(self.PlotImage)
-        #todo set slider position to middle and also disp middle image
+        # todo set slider position to middle and also disp middle image
         self.TimeSliderTimepoint = QLabel()
 
         ZPositionSiderLabel = QLabel('z-position')
         self.ZPosition_Slider = QSlider(Qt.Orientation.Horizontal, self)
-        self.ZPosition_Slider.setTickInterval(1)
-        self.ZPosition_Slider.valueChanged.connect(self.updateSliderLabel)
+
         self.ZPosition_Slider.valueChanged.connect(self.PlotImage)
         self.ZStackSliderPosition = QLabel()
 
@@ -480,9 +481,74 @@ class qtGUI(QDialog, GUIFunctions):
 
         self.PlotOptionBox.setLayout(layout)
 
+    def createDetectCircleBox(self):
+        self.DetectCircleBox = QGroupBox('Detect GUVs')
+
+        Param_Instructions = QLabel('Adjust the parameters to detect GUVs \n'
+                                    'the bigger param1, the fewer circles may be detected \n'
+                                    'the smaller param2, the more false circle sare detected')
+
+        # todo sync this with a text box
+        Param1SliderLabel = QLabel('param1')
+        self.Param1_Slider = QSlider(Qt.Orientation.Horizontal, self)
+        self.Param1_Slider.setRange(1, 200)
+        self.Param1_Slider.setValue(10)
+        self.Param1_Slider.setTickInterval(1)
+        self.Param1_Slider.valueChanged.connect(self.updateSliderLabel)
+        self.Param1_SliderValue = QLabel()
+
+        Param2SliderLabel = QLabel('param2')
+        self.Param2_Slider = QSlider(Qt.Orientation.Horizontal, self)
+        self.Param2_Slider.setRange(1, 250)
+        self.Param2_Slider.setValue(150)
+        self.Param2_Slider.setTickInterval(1)
+        self.Param2_Slider.valueChanged.connect(self.updateSliderLabel)
+        self.Param2_SliderValue = QLabel()
+
+        SizeLabel = QLabel('GUV size [px] (min, max)')
+        self.SizeMin = QLineEdit('45')
+        self.SizeMax = QLineEdit('60')
+
+        DisplayChannelLabel = QLabel('Display Channel')
+        self.DetectDisplayChannel = QComboBox()
+
+        DetectChannelLabel = QLabel('Detection Channel')
+        self.DetectDetectChannel = QComboBox()
+
+        self.DetectCircleButton = QPushButton('Detect GUVs on current image')
+        self.DetectCircleButton.clicked.connect(self.detectGUVs)
+
+        self.DetectAllCirclesButton = QPushButton('Detect GUVs on all images')
+
+        layout = QGridLayout()
+        layout.addWidget(Param_Instructions, 0, 0)
+
+        layout.addWidget(Param1SliderLabel, 1, 0)
+        layout.addWidget(self.Param1_Slider, 1, 1)
+        layout.addWidget(self.Param1_SliderValue, 1, 2)
+
+        layout.addWidget(Param2SliderLabel, 2, 0)
+        layout.addWidget(self.Param2_Slider, 2, 1)
+        layout.addWidget(self.Param2_SliderValue, 2, 2)
+
+        layout.addWidget(SizeLabel, 3, 0)
+        layout.addWidget(self.SizeMin, 3, 1)
+        layout.addWidget(self.SizeMax, 3, 2)
+
+        layout.addWidget(DisplayChannelLabel, 4, 0)
+        layout.addWidget(self.DetectDisplayChannel, 4, 1)
+
+        layout.addWidget(DetectChannelLabel, 5, 0)
+        layout.addWidget(self.DetectDetectChannel, 5, 1)
+
+        layout.addWidget(self.DetectCircleButton, 6, 0)
+        layout.addWidget(self.DetectAllCirclesButton, 6, 1)
+
+        self.DetectCircleBox.setLayout(layout)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     app.setStyle('Fusion')
     input_window = qtGUI()
     input_window.exec()
-
